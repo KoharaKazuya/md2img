4d1e885c4cfa01701b92ba63b5a0a4c17bb32f75
KoharaKazuya <reflect.results@gmail.com>
2017-09-07 23:23:29 +0900

    add yargs

--- bin/md2img.js
+++ bin/md2img.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
-var md2img = require('../lib/index');
+const md2img = require('../lib/index');
+const yargs = require('yargs');
+
+const args = yargs.argv._;
 
 // コマンドの使い方を表示
 function showUsage() {
@@ -9,18 +12,18 @@ function showUsage() {
 }
 
 // コマンドライン引数が 2 つ以上だった場合、中断
-if (process.argv.length > 3) {
+if (args.length > 1) {
   showUsage();
 }
 
 // コマンドライン引数が渡されていない場合、デフォルトとして README.md を引数とする
-var sourceFile;
-if (process.argv.length === 3) {
-  sourceFile = process.argv[2];
+let sourceFile;
+if (args.length === 1) {
+  [sourceFile] = args;
 } else {
   sourceFile = 'README.md';
 }
 
-md2img(sourceFile, function(contents) {
+md2img(sourceFile, (contents) => {
   process.stdout.write(contents);
 });
