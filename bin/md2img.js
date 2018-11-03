494be8f2b2d4f59ee2231ec3a384e6362422e72a
KoharaKazuya <reflect.results@gmail.com>
2018-11-03 20:03:33 +0900

    lynt and prettier

--- bin/md2img.js
+++ bin/md2img.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
-const md2img = require('../lib/index');
-const yargs = require('yargs');
+const md2img = require("../lib/index");
+const yargs = require("yargs");
 
 const argv = yargs
-  .alias('h', 'help')
-  .usage('$0 [md]', 'Convert markdown file into image', yargs => {
-    yargs.positional('md', {
-      describe: 'a markdown file',
-      type: 'string'
+  .alias("h", "help")
+  .usage("$0 [md]", "Convert markdown file into image", yargs => {
+    yargs.positional("md", {
+      describe: "a markdown file",
+      type: "string"
     });
   }).argv;
 
 // コマンドライン引数が渡されていない場合、デフォルトとして README.md を引数とする
-const sourceFile = argv.md || 'README.md';
+const sourceFile = argv.md || "README.md";
 md2img(sourceFile, contents => {
   process.stdout.write(contents);
 });
