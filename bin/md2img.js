d21406c126b1a87e21115a69a04b9f1f2221fbe0
KoharaKazuya <reflect.results@gmail.com>
2018-11-03 19:50:35 +0900

    add CLI help

--- bin/md2img.js
+++ bin/md2img.js
@@ -3,27 +3,17 @@
 const md2img = require('../lib/index');
 const yargs = require('yargs');
 
-const args = yargs.argv._;
-
-// コマンドの使い方を表示
-function showUsage() {
-  console.error('TODO');
-  process.exit();
-}
-
-// コマンドライン引数が 2 つ以上だった場合、中断
-if (args.length > 1) {
-  showUsage();
-}
+const argv = yargs
+  .alias('h', 'help')
+  .usage('$0 [md]', 'Convert markdown file into image', yargs => {
+    yargs.positional('md', {
+      describe: 'a markdown file',
+      type: 'string'
+    });
+  }).argv;
 
 // コマンドライン引数が渡されていない場合、デフォルトとして README.md を引数とする
-let sourceFile;
-if (args.length === 1) {
-  [sourceFile] = args;
-} else {
-  sourceFile = 'README.md';
-}
-
-md2img(sourceFile, (contents) => {
+const sourceFile = argv.md || 'README.md';
+md2img(sourceFile, contents => {
   process.stdout.write(contents);
 });
